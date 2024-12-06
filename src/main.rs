e5406edb596a015298c39477f60e0b1ae9c6c115
kiminonawa <>
2024-12-06 23:28:40 +0100

    day5

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,47 @@ fn main() {
 
     let tmp = raw_data.split("\n\n").collect::<Vec<&str>>();
 
-    let rules = tmp[0];
+    let rules = tmp[0].split("\n").collect::<Vec<&str>>();
+
+    let mut res = 0;
 
     for prints in tmp[1].split("\n") {
         //create opposing rule
 
-        for pages in prints.split(",") {
-            
+        let mut numbers = prints.split(",").collect::<Vec<&str>>();
+
+        let mut valid = true;
+
+        let mut i = 1;
+        let mut j = 0;
+
+        while i < numbers.len() {
+            'inner: while j < i {
+                if rules.contains(&format!("{}|{}", numbers[i], numbers[j]).as_str()) {
+                    valid = false;
+                    // element i muss dann vor j gepackt werden
+                    let tmp = numbers.remove(i);
+                    numbers.insert(j, tmp);
+                    i = 0;
+                    j = 0;
+                    break 'inner
+                }
+                j += 1;
+            }
+            i += 1;
         }
+        if !valid {
+            res += numbers[numbers.len()/2].parse::<i32>().unwrap();
+        }
+        /*
+        println!("opposing rules {:?}", opposing_rules);
 
-        println!("{:?}", rules);
+        println!("rules {:?}", rules);
 
-        println!("{:?}", prints);
+        println!(" thing tried {:?}", prints);
+        */
         //check for exisiting rules
     }
+
+    println!("{res}");
 }
