6940d609322892a5be783e79afce74fe01698fff
kiminonawa <>
2024-12-11 10:29:04 +0100

    day6

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,6 @@ fn main() {
         maze.push(i.chars().collect::<Vec<char>>());
     }
 
-    let mut direction = 0; //0 up, 1 right, 2 down, 3 left mod 4
-
     let move_direction: [(i32, i32); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
 
     let mut position = (0, 0);
@@ -32,48 +30,61 @@ fn main() {
 
     let mut visited = 0;
 
-    'walk: loop {
+    let mut part2 = 0;
 
-        let next_field = (position.0+move_direction[direction].0, position.1+move_direction[direction].1);
+    for i in 0..maze.len() {
+        for j in 0..maze[i].len() {
+            //println!("{}",maze.len());
+            //println!("{}",maze[0].len());
+            let mut tmp_maze = maze.clone();
+            tmp_maze[i][j]='#';
+            let mut tmp_position = position.clone();
+            let mut direction = 0; //0 up, 1 right, 2 down, 3 left mod 4
 
-        if next_field.0 < 0 || next_field.0 > maze.len() as i32 -1 || next_field.1 < 0 || next_field.1 > maze[0].len() as i32 - 1{
+            'walk: loop {
 
-            if maze[position.0 as usize][position.1 as usize].ne(&'X') {
-                maze[position.0 as usize][position.1 as usize] = 'X';
-                visited += 1;
+                let next_field = (tmp_position.0+move_direction[direction].0, tmp_position.1+move_direction[direction].1);
+
+                if next_field.0 < 0 || next_field.0 > tmp_maze.len() as i32 -1 || next_field.1 < 0 || next_field.1 > tmp_maze[0].len() as i32 - 1{
+
+                    if tmp_maze[tmp_position.0 as usize][tmp_position.1 as usize].ne(&'X') {
+                        tmp_maze[tmp_position.0 as usize][tmp_position.1 as usize] = 'X';
+                        visited += 1;
+                    }
+
+                    break 'walk;
+                }
+
+                let next_object = tmp_maze[next_field.0 as usize][next_field.1 as usize];
+
+                if next_object.eq(&'#') {
+                    direction = (direction+1)%4;
+                } else {
+                    if tmp_maze[tmp_position.0 as usize][tmp_position.1 as usize].ne(&'X') {
+                        tmp_maze[tmp_position.0 as usize][tmp_position.1 as usize] = 'X';
+                        visited += 1;
+                        not_visited = 0;
+
+                    } else {
+                        not_visited += 1;
+                    }
+                    tmp_position = next_field;
+
+                }
+
+                if not_visited > 130*4 {
+                    part2 += 1;
+                    break 'walk;
+                }
             }
-
-            break 'walk;
-        }
-
-        let next_object = maze[next_field.0 as usize][next_field.1 as usize];
-
-        if next_object.eq(&'#') {
-            direction = (direction+1)%4;
-        } else {
-            if maze[position.0 as usize][position.1 as usize].ne(&'X') {
-                maze[position.0 as usize][position.1 as usize] = 'X';
-                visited += 1;
-                not_visited = 0;
-
-            } else {
-                not_visited += 1;
-            }
-            position = next_field;
-
-        }
-
-        if not_visited > 130*4 {
-            break 'walk;
         }
     }
-    for i in maze {
-        println!("{:?}", i);
-    }
+    //for i in maze {
+    //    println!("{:?}", i);
+    //}
     println!("{visited}");
+    println!("{part2}");
 
-    //was ist mein workflow: -> finde den guard, starting direction ist up -> setze x auf aktueller position visited +1 (falls x schon gesetzt pass)
-    // -> addiere entsprechend der richtung
 
     
 }
